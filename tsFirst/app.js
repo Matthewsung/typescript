7c3b2d66f4258b175555877023964b49231707f1
juyeongsung <jooyongs1@gmail.com>
2021-08-26 18:35:36 +0900

    8.26 first

---
+++ tsFirst/app.js
@@ -0,0 +1,7 @@
+"use strict";
+function logName(name) {
+    console.log(name);
+}
+logName('Jack');
+var a = 5;
+a = 10;
---
+++ tsFirst/app.ts
@@ -0,0 +1,10 @@
+function logName (name:string){
+  console.log(name)
+}
+
+logName('Jack');
+
+
+let a = 5
+
+a = 10; 
